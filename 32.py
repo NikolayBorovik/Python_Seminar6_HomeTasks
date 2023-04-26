ab41dfb314014c9af5be58d2f99b960b8bea232e
Nikolay Borovik <nik.Borovik@bk.ru>
2023-04-26 13:41:58 +1000

    32 done

--- 32.py
+++ 32.py
@@ -2,4 +2,19 @@
 # значения которых принадлежат заданному диапазону (т.е. не
 # меньше заданного минимума и не больше заданного
 # максимума)
+from random import randint
 
+size = int(input('Enter size of the list: '))
+min = int(input('Enter min in the range: '))
+max = int(input('Enter max in the range: '))
+
+list =[]
+for i in range(size):
+    list.append(randint(0,9))
+    print(i, end='  ')
+print(' ')
+print(list)
+
+for i in range(len(list)):
+    if list[i] >=min and list[i] <= max:
+        print(i, end=' ')
